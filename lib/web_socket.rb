ec0ae1d0ac9901ef254cbbaf66ee8cc4eae8151e
Marv Cool <marv@hostin.is>
2012-12-17 21:24:02 +0100

    removes server side code

--- lib/web_socket.rb
+++ lib/web_socket.rb
@@ -1,10 +1,3 @@
-# Copyright: Hiroshi Ichikawa <http://gimite.net/en/>
-# Lincense: New BSD Lincense
-# Reference: http://tools.ietf.org/html/draft-hixie-thewebsocketprotocol-75
-# Reference: http://tools.ietf.org/html/draft-hixie-thewebsocketprotocol-76
-# Reference: http://tools.ietf.org/html/draft-ietf-hybi-thewebsocketprotocol-07
-# Reference: http://tools.ietf.org/html/draft-ietf-hybi-thewebsocketprotocol-10
-
 require "base64"
 require "socket"
 require "uri"
@@ -35,42 +28,7 @@ class WebSocket
     OPCODE_PONG = 0x0a
 
     def initialize(arg, params = {})
-      if params[:server] # server
 
-        @server = params[:server]
-        @socket = arg
-        line = gets()
-        if !line
-          raise(WebSocket::Error, "Client disconnected without sending anything.")
-        end
-        line = line.chomp()
-        if !(line =~ /\AGET (\S+) HTTP\/1.1\z/n)
-          raise(WebSocket::Error, "Invalid request: #{line}")
-        end
-        @path = $1
-        read_header()
-        if @header["sec-websocket-version"]
-          @web_socket_version = @header["sec-websocket-version"]
-          @key3 = nil
-        elsif @header["sec-websocket-key1"] && @header["sec-websocket-key2"]
-          @web_socket_version = "hixie-76"
-          @key3 = read(8)
-        else
-          @web_socket_version = "hixie-75"
-          @key3 = nil
-        end
-        if !@server.accepted_origin?(self.origin)
-          raise(WebSocket::Error,
-            ("Unaccepted origin: %s (server.accepted_domains = %p)\n\n" +
-              "To accept this origin, write e.g. \n" +
-              "  WebSocketServer.new(..., :accepted_domains => [%p]), or\n" +
-              "  WebSocketServer.new(..., :accepted_domains => [\"*\"])\n") %
-            [self.origin, @server.accepted_domains, @server.origin_to_domain(self.origin)])
-        end
-        @handshaked = false
-
-      else # client
-        
         @web_socket_version = "hixie-76"
         uri = arg.is_a?(String) ? URI.parse(arg) : arg
 
@@ -124,7 +82,6 @@ class WebSocket
         end
         @handshaked = true
 
-      end
       @received = []
       @buffer = ""
       @closing_started = false
@@ -436,153 +393,4 @@ class WebSocket
       return ssl_socket
     end
 
-end
-
-
-class WebSocketServer
-
-    def initialize(params_or_uri, params = nil)
-      if params
-        uri = params_or_uri.is_a?(String) ? URI.parse(params_or_uri) : params_or_uri
-        params[:port] ||= uri.port
-        params[:accepted_domains] ||= [uri.host]
-      else
-        params = params_or_uri
-      end
-      @port = params[:port] || 80
-      @accepted_domains = params[:accepted_domains]
-      if !@accepted_domains
-        raise(ArgumentError, "params[:accepted_domains] is required")
-      end
-      if params[:host]
-        @tcp_server = TCPServer.open(params[:host], @port)
-      else
-        @tcp_server = TCPServer.open(@port)
-      end
-    end
-
-    attr_reader(:tcp_server, :port, :accepted_domains)
-
-    def run(&block)
-      while true
-        Thread.start(accept()) do |s|
-          begin
-            ws = create_web_socket(s)
-            yield(ws) if ws
-          rescue => ex
-            print_backtrace(ex)
-          ensure
-            begin
-              ws.close_socket() if ws
-            rescue
-            end
-          end
-        end
-      end
-    end
-
-    def accept()
-      return @tcp_server.accept()
-    end
-
-    def accepted_origin?(origin)
-      domain = origin_to_domain(origin)
-      return @accepted_domains.any?(){ |d| File.fnmatch(d, domain) }
-    end
-
-    def origin_to_domain(origin)
-      if origin == "null" || origin == "file://"  # local file
-        return "null"
-      else
-        return URI.parse(origin).host
-      end
-    end
-
-    def create_web_socket(socket)
-      ch = socket.getc()
-      if ch == ?<
-        # This is Flash socket policy file request, not an actual Web Socket connection.
-        send_flash_socket_policy_file(socket)
-        return nil
-      else
-        socket.ungetc(ch) if ch
-        return WebSocket.new(socket, :server => self)
-      end
-    end
-
-  private
-
-    def print_backtrace(ex)
-      $stderr.printf("%s: %s (%p)\n", ex.backtrace[0], ex.message, ex.class)
-      for s in ex.backtrace[1..-1]
-        $stderr.printf("        %s\n", s)
-      end
-    end
-
-    # Handles Flash socket policy file request sent when web-socket-js is used:
-    # http://github.com/gimite/web-socket-js/tree/master
-    def send_flash_socket_policy_file(socket)
-      socket.puts('<?xml version="1.0"?>')
-      socket.puts('<!DOCTYPE cross-domain-policy SYSTEM ' +
-        '"http://www.macromedia.com/xml/dtds/cross-domain-policy.dtd">')
-      socket.puts('<cross-domain-policy>')
-      for domain in @accepted_domains
-        next if domain == "file://"
-        socket.puts("<allow-access-from domain=\"#{domain}\" to-ports=\"#{@port}\"/>")
-      end
-      socket.puts('</cross-domain-policy>')
-      socket.close()
-    end
-
-end
-
-
-if __FILE__ == $0
-  Thread.abort_on_exception = true
-
-  if ARGV[0] == "server" && ARGV.size == 3
-
-    server = WebSocketServer.new(
-      :accepted_domains => [ARGV[1]],
-      :port => ARGV[2].to_i())
-    puts("Server is running at port %d" % server.port)
-    server.run() do |ws|
-      puts("Connection accepted")
-      puts("Path: #{ws.path}, Origin: #{ws.origin}")
-      if ws.path == "/"
-        ws.handshake()
-        while data = ws.receive()
-          printf("Received: %p\n", data)
-          ws.send(data)
-          printf("Sent: %p\n", data)
-        end
-      else
-        ws.handshake("404 Not Found")
-      end
-      puts("Connection closed")
-    end
-
-  elsif ARGV[0] == "client" && ARGV.size == 2
-
-    client = WebSocket.new(ARGV[1])
-    puts("Connected")
-    Thread.new() do
-      while data = client.receive()
-        printf("Received: %p\n", data)
-      end
-    end
-    $stdin.each_line() do |line|
-      data = line.chomp()
-      client.send(data)
-      printf("Sent: %p\n", data)
-    end
-
-  else
-
-    $stderr.puts("Usage:")
-    $stderr.puts("  ruby web_socket.rb server ACCEPTED_DOMAIN PORT")
-    $stderr.puts("  ruby web_socket.rb client ws://HOST:PORT/")
-    exit(1)
-
-  end
 end
